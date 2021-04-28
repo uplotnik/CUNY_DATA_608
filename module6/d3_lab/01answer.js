83d6e17e59231840102ab2582bfa9004e6fba747
uplotnik <46010159+uplotnik@users.noreply.github.com>
2021-04-28 13:29:06 -0700

    Update 01answer.js

--- module6/d3_lab/01answer.js
+++ module6/d3_lab/01answer.js
@@ -1,8 +1,28 @@
-d3.csv('ue_industry.csv', data => {
+d3.csv(''https://github.com/uplotnik/CUNY_DATA_608/blob/master/module6/d3_lab/ue_industry.csv', data => {
 
     // Define your scales and generator here.
+  console.log(data);
 
-    d3.select('#answer1')
+    
+    const xScale = d3.scaleLinear()
+        .domain(d3.extent(data, d => +d.index))
+        .range([1190, 25]);
+    
+    const yScale = d3.scaleLinear()
+        .domain(d3.extent(data, d => +d.Agriculture))
+        .range([570, 18]);
+    
+    let line10 = d3.line()
+        .x(d => xScale(d.index))
+        .y(d => yScale(d.Agriculture));
+    
+d3.select('#answer1')
         // append more elements here
-
+      .selectAll('path')
+        .data(data)
+        .enter()        
+        .append('path')
+        .attr('d', line10(data))
+        .attr("stroke-width", 1.8)
+        .attr("stroke", "darkblue")
 });
